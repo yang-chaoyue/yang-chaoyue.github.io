79a947a817c47769c8cc451d2cbc5c6eb668a5af
ycyh <ycyh.master@gmail.com>
2018-08-08 23:26:03 -0700

    kaluli

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # yang-chaoyue.github.io
+# 燃烧我的卡路里
---
+++ ycyhApp.js
@@ -0,0 +1,38 @@
+"use strict";
+var app = angular.module('ycyh', []);
+app.controller('ycyhCtrl', function($scope, $http, $interval) {
+  $scope.count = 6;
+  $scope.callGetCount = function() {
+    $http({
+        method : "GET",
+        url : "https://kb1jwi4uvg.execute-api.us-west-1.amazonaws.com/prod01/addone"
+      }).then(
+        function(response) {
+          if (response.data[2].cnt > $scope.count) {
+            $scope.count = response.data[2].cnt;
+          }
+        }, function(response) {
+          console.log(response);
+        }
+    );
+  }
+  $scope.callGetCount();
+  $interval($scope.callGetCount, 3000);
+
+  $scope.play1 = function() {
+    $scope.count = $scope.count + 1;
+    console.log($("#audioDiv"));
+    $("#audioDiv").children()[0].play();
+    $http({
+      method : "POST",
+      url : "https://kb1jwi4uvg.execute-api.us-west-1.amazonaws.com/prod01/addone",
+      data: { "name" : "kll" }
+    }).then(
+      function(response) {
+      }, function(response) {
+        console.log(response);
+      }
+    );
+  }
+
+});
